7e62fe62507af5c3966e23ede2df10b9224c076c
zhouyao <465033064@qq.com>
2015-12-05 23:25:20 +0800

    .

--- application/controllers/sport.php
+++ application/controllers/sport.php
@@ -8,22 +8,73 @@
 class sport extends CI_Controller
 {
     public function userindex(){
-        $this->load->view("sport/index");
+        $this->load->model("Sport_model");
+        $userId =$_SESSION['userId'];
+        $data=$this->Sport_model->gethealthdatabyId($userId);
+        $this->load->view("sport/index",array('data'=>$data));
     }
 
     public function administration(){
-        $this->load->view("sport/administration");
+        $this->load->model("Sport_model");
+        $userId =$_SESSION['userId'];
+        $data=$this->Sport_model->gethealthdatabyId($userId);
+        $this->load->view("sport/administration",array('data'=>$data));
     }
 
     public function track(){
-        $this->load->view("sport/track");
+        $this->load->model("Sport_model");
+        $userId =$_SESSION['userId'];
+        $data=$this->Sport_model->gethealthdatabyId($userId);
+        $this->load->view("sport/track",array('data'=>$data));
     }
 
     public function sleepAnalysis(){
-        $this->load->view("sport/sleepAnalysis");
+        $this->load->model("Sport_model");
+        $userId =$_SESSION['userId'];
+        $data=$this->Sport_model->gethealthdatabyId($userId);
+        $this->load->view("sport/sleepAnalysis",array('data'=>$data));
     }
 
     public function ride(){
-        $this->load->view("sport/ride");
+        $this->load->model("Sport_model");
+        $userId =$_SESSION['userId'];
+        $data=$this->Sport_model->gethealthdatabyId($userId);
+        $this->load->view("sport/ride",array('data'=>$data));
+    }
+
+
+    public function initaldata(){
+        $this->load->model("Sport_model");
+        for($i=0;$i<500;$i++){
+            $userId = 9;
+            $healthsleep = 0.80;
+            $usefulexercise = 0.70;
+            $sleeptime = rand(6, 9);
+            $exercisetime = rand(0, 5);
+            $data=array(
+                'userId' => $userId,
+                'height' => 180,
+                'weight' => 70,
+                'BMI' => 21.60,
+                'healthsleep' => $healthsleep,
+                'usefulexercise' => $usefulexercise,
+                'sleeptime' => $sleeptime,
+                'exercisetime' => $exercisetime,
+            );
+            $this->Sport_model->inserthealth($data);
+        }
+        echo "success";
+    }
+
+
+    public function adddata(){
+        $simple = "<para><note>simple note</note></para>";
+        $p = xml_parser_create();
+        xml_parse_into_struct($p, $simple, $vals, $index);
+        xml_parser_free($p);
+        echo "Index array\n";
+        print_r($index);
+        echo "\nVals array\n";
+        print_r($vals);
     }
 }
---
+++ application/models/sport_model.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2015/12/5
+ * Time: 下午22:23
+ */
+class sport_model  extends CI_Model
+{
+    public function inserthealth($data){
+        return $this->db->insert("healthdata",$data);
+    }
+
+    public function gethealthdatabyId($userId){
+        $res=$this->db
+            ->from('healthdata')
+            ->where('userId',$userId)
+            ->order_by('healthdataId','desc')
+            ->limit('0,10')
+            ->get();
+        return $res->result();
+    }
+}
